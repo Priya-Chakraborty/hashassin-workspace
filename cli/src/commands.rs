f6d62bc05faa9bdd065976f321154cd5bc64d4fb
Priya Chakraborty <chakrabortypriya730@gmial.com>
2025-03-24 15:29:43 -0400

    Update project with gen-hashes and dump-hashes commands

--- cli/src/commands.rs
+++ cli/src/commands.rs
@@ -1,30 +1,25 @@
-use hashassin_core::{
-    hash::{Algorithm, hash_password},
-    password_gen,
-};
+use hashassin_core::{password_gen, hash::{hash_password, Algorithm}};
 use rayon::prelude::*;
 use std::{env, fs, io::Write, path::PathBuf};
 
+/// Parse the algorithm flag into our Algorithm enum.
 pub fn parse_algorithm(alg: &str) -> Algorithm {
     match alg.to_lowercase().as_str() {
-        "sha256" => Algorithm::SHA256,
-        "sha3-512" => Algorithm::SHA3_512,
-        "md5" => Algorithm::MD5,
-        "scrypt" => Algorithm::Scrypt,
+        "sha256"    => Algorithm::SHA256,
+        "sha3-512"  => Algorithm::SHA3_512,
+        "md5"       => Algorithm::MD5,
+        "scrypt"    => Algorithm::Scrypt,
         _ => panic!("Unknown algorithm"),
     }
 }
 
+/// Generates a list of random passwords and outputs each with its hash.
 pub fn gen_passwords(num: usize, chars: usize, threads: usize, algorithm: &str, out_file: &str) {
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(threads)
-        .build_global()
-        .unwrap();
+    rayon::ThreadPoolBuilder::new().num_threads(threads).build_global().unwrap();
     let alg = parse_algorithm(algorithm);
 
     let passwords = password_gen::generate_multiple_passwords(num, chars);
-    let hashes: Vec<String> = passwords
-        .par_iter()
+    let hashes: Vec<String> = passwords.par_iter()
         .map(|p| format!("{}:{}", p, hash_password(p, alg)))
         .collect();
 
@@ -35,20 +30,15 @@ pub fn gen_passwords(num: usize, chars: usize, threads: usize, algorithm: &str, 
     println!("Passwords and hashes written to {}", out_file);
 }
 
+/// Checks each password from the given file against the provided hash dump.
 pub fn check_passwords(hash_file: &str, password_file: &str, algorithm: &str) {
     let alg = parse_algorithm(algorithm);
+    let current_dir = env::current_dir().unwrap();
 
-    let mut hash_path = PathBuf::from(env::current_dir().unwrap());
-    hash_path.push(hash_file);
-
-    let mut password_path = PathBuf::from(env::current_dir().unwrap());
-    password_path.push(password_file);
-
-    println!("Debug - Hash path: {:?}", hash_path);
-    println!("Debug - Password path: {:?}", password_path);
-
-    let hashes = fs::read_to_string(&hash_path).expect("Hash file not found");
-    let passwords = fs::read_to_string(&password_path).expect("Password file not found");
+    let hashes = fs::read_to_string(current_dir.join(hash_file))
+        .expect("Hash file not found");
+    let passwords = fs::read_to_string(current_dir.join(password_file))
+        .expect("Password file not found");
 
     let hash_set: Vec<&str> = hashes.lines().collect();
     let password_list: Vec<&str> = passwords.lines().collect();
@@ -58,5 +48,36 @@ pub fn check_passwords(hash_file: &str, password_file: &str, algorithm: &str) {
         if hash_set.contains(&hashed.as_str()) {
             println!("MATCH FOUND: Password '{}' hashes to '{}'", pass, hashed);
         }
+    }
+}
+
+/// Reads an input file of passwords, computes hashes using the specified algorithm (with multithreading), and writes the results to an output file.
+pub fn gen_hashes(in_file: &str, out_file: &str, threads: usize, algorithm: &str) {
+    rayon::ThreadPoolBuilder::new().num_threads(threads).build_global().unwrap();
+    let alg = parse_algorithm(algorithm);
+    let current_dir = env::current_dir().unwrap();
+
+    let passwords = fs::read_to_string(current_dir.join(in_file))
+        .expect("Input file not found");
+    let password_list: Vec<&str> = passwords.lines().collect();
+
+    let hashes: Vec<String> = password_list.par_iter()
+        .map(|p| format!("{}:{}", p, hash_password(p, alg)))
+        .collect();
+
+    let mut file = fs::File::create(out_file).unwrap();
+    for line in hashes {
+        writeln!(file, "{line}").unwrap();
+    }
+    println!("Hashes written to {}", out_file);
+}
+
+/// Dumps the contents of a hash file to standard output.
+pub fn dump_hashes(hash_file: &str) {
+    let current_dir = env::current_dir().unwrap();
+    let hashes = fs::read_to_string(current_dir.join(hash_file))
+        .expect("Hash file not found");
+    for line in hashes.lines() {
+        println!("{}", line);
     }
 }
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,6 @@
 mod commands;
 use clap::{Parser, Subcommand};
-use commands::{check_passwords, gen_passwords};
+use commands::{gen_passwords, check_passwords, gen_hashes, dump_hashes};
 
 #[derive(Parser)]
 #[command(name = "hashassin", version, author, about)]
@@ -11,6 +11,7 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Generate passwords and their hashes.
     GenPasswords {
         #[arg(long)]
         num: usize,
@@ -23,6 +24,7 @@ enum Commands {
         #[arg(long)]
         out_file: String,
     },
+    /// Check passwords against a hash dump.
     CheckPasswords {
         #[arg(long)]
         hash_file: String,
@@ -31,27 +33,39 @@ enum Commands {
         #[arg(long)]
         algorithm: String,
     },
+    /// Generate hashes from an input file of passwords.
+    GenHashes {
+        #[arg(long)]
+        in_file: String,
+        #[arg(long)]
+        out_file: String,
+        #[arg(long)]
+        threads: usize,
+        #[arg(long)]
+        algorithm: String,
+    },
+    /// Dump (print) hashes from a given hash file.
+    DumpHashes {
+        #[arg(long)]
+        hash_file: String,
+    },
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::GenPasswords {
-            num,
-            chars,
-            threads,
-            algorithm,
-            out_file,
-        } => {
+        Commands::GenPasswords { num, chars, threads, algorithm, out_file } => {
             gen_passwords(*num, *chars, *threads, algorithm, out_file);
-        }
-        Commands::CheckPasswords {
-            hash_file,
-            password_file,
-            algorithm,
-        } => {
+        },
+        Commands::CheckPasswords { hash_file, password_file, algorithm } => {
             check_passwords(hash_file, password_file, algorithm);
-        }
+        },
+        Commands::GenHashes { in_file, out_file, threads, algorithm } => {
+            gen_hashes(in_file, out_file, *threads, algorithm);
+        },
+        Commands::DumpHashes { hash_file } => {
+            dump_hashes(hash_file);
+        },
     }
 }
